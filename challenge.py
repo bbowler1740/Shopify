2b15de1380494ffc3149745d4b694c12d9ee2c8d
Brayden <bbowler1740@conestogac.on.ca>
2022-01-12 13:50:21 -0500

    first commit

---
+++ challenge.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+def calculate_avg__order_amount():
+    data = pd.read_excel (r'C:\Users\Brayden\Desktop\Personal\Applications\Shopify\2019 Winter Data Science Intern Challenge Data Set.xlsx') #Use a relative path.
+    df = pd.DataFrame(data, columns=['order_amount'])
+    x = df.sum()
+    y = (x / df.size) #Size calculates elements in the df object. Represents number of rows in the Excel sheet (or in the business case, total orders).
+    return y
+
+def calculate_avg_item_cost_per_order():
+    data = pd.read_excel (r'C:\Users\Brayden\Desktop\Personal\Applications\Shopify\2019 Winter Data Science Intern Challenge Data Set.xlsx') #Use a relative path.
+    dfTI = pd.DataFrame(data, columns=['total_items'])
+    dfOA = pd.DataFrame(data, columns=['order_amount'])
+    sumTotalItems = float(dfTI.sum())
+    sumOrderAmount = float(dfOA.sum())
+    avgCost = float(sumOrderAmount / sumTotalItems) #Represents the avg item cost per order
+    return avgCost
+
+print(calculate_avg__order_amount()) #We know from the case description this should be $3,145.13, use as a check.
+print(calculate_avg_item_cost_per_order())
+
